9dcd240683b5556a5919b0668e50d9a4d2d46300
shikiteru <riidho808@gmail.com>
2025-09-10 13:55:33 +0700

    mycash-v2 add fix redirect

--- middleware.ts
+++ middleware.ts
@@ -7,46 +7,41 @@ const ALLOWED_ORIGINS = [
 ];
 
 export function middleware(req: NextRequest) {
-  const origin = req.headers.get("origin") || "";
-
-  if (!req.nextUrl.pathname.startsWith("/api")) {
-    return NextResponse.next();
-  }
-
-  const allowedOrigin = ALLOWED_ORIGINS.includes(origin) ? origin : "";
-
-  if (req.method === "OPTIONS") {
-    const res = new NextResponse(null, { status: 204 });
-    if (allowedOrigin) {
-      res.headers.set("Access-Control-Allow-Origin", allowedOrigin);
-    }
-    res.headers.set(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    res.headers.set(
-      "Access-Control-Allow-Headers",
-      "Content-Type, Authorization"
-    );
-    res.headers.set("Access-Control-Allow-Credentials", "true");
-    return res;
-  }
-
-  const res = NextResponse.next();
-  if (allowedOrigin) {
-    res.headers.set("Access-Control-Allow-Origin", allowedOrigin);
-  }
-  res.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-  res.headers.set("Access-Control-Allow-Credentials", "true");
-
-  return res;
+  //   const origin = req.headers.get("origin") || "";
+  //   if (!req.nextUrl.pathname.startsWith("/api")) {
+  //     return NextResponse.next();
+  //   }
+  //   const allowedOrigin = ALLOWED_ORIGINS.includes(origin) ? origin : "";
+  //   if (req.method === "OPTIONS") {
+  //     const res = new NextResponse(null, { status: 204 });
+  //     if (allowedOrigin) {
+  //       res.headers.set("Access-Control-Allow-Origin", allowedOrigin);
+  //     }
+  //     res.headers.set(
+  //       "Access-Control-Allow-Methods",
+  //       "GET, POST, PUT, DELETE, OPTIONS"
+  //     );
+  //     res.headers.set(
+  //       "Access-Control-Allow-Headers",
+  //       "Content-Type, Authorization"
+  //     );
+  //     res.headers.set("Access-Control-Allow-Credentials", "true");
+  //     return res;
+  //   }
+  //   const res = NextResponse.next();
+  //   if (allowedOrigin) {
+  //     res.headers.set("Access-Control-Allow-Origin", allowedOrigin);
+  //   }
+  //   res.headers.set(
+  //     "Access-Control-Allow-Methods",
+  //     "GET, POST, PUT, DELETE, OPTIONS"
+  //   );
+  //   res.headers.set(
+  //     "Access-Control-Allow-Headers",
+  //     "Content-Type, Authorization"
+  //   );
+  //   res.headers.set("Access-Control-Allow-Credentials", "true");
+  //   return res;
 }
 
 export const config = {
